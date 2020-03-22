5d7a4af35bbcdaebdfc5499b794af0e4629be1e1
Jeremy Boy <github@jboy.eu>
2020-03-22 20:11:42 +0100

    remove styles

--- expo-app/App.tsx
+++ expo-app/App.tsx
@@ -1,24 +1,7 @@
 import React from "react";
-import { StyleSheet} from "react-native";
 import Navigator from "./src/navigation";
 
 
 export default function App() {
     return <Navigator />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-  },
-  leftContainer: {
-    flex: 1,
-  },
-  midContainer: {
-    flex: 2,
-  },
-  rightContainer: {
-    flex: 1,
-  }
-});
