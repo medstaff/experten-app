a314ebacc06b4013fb605edcc03b5ec4a54bdb61
Yannick Streicher <yannick.streicher@uni.kn>
2020-03-22 21:23:34 +0100

    Updates

---
+++ precache-manifest.6a818c52294df8706d3c2cab6ec5b70e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/helfer-app/favicon.ico"
+  },
+  {
+    "revision": "ce9b8d9a72903950f794",
+    "url": "/helfer-app/static/js/app.4c1f6ba8.chunk.js"
+  },
+  {
+    "revision": "73dd6e1f9f36b50b1699",
+    "url": "/helfer-app/static/js/runtime~app.ba893389.js"
+  },
+  {
+    "revision": "66f93f56be7ddcec35d4",
+    "url": "/helfer-app/static/js/2.ad603114.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/helfer-app/serve.json"
+  },
+  {
+    "revision": "314f0f89799ea162aa49da4b9c658a05",
+    "url": "/helfer-app/register-service-worker.js"
+  },
+  {
+    "revision": "3f78b1ad69a2aab300789bcb707ece2f",
+    "url": "/helfer-app/manifest.json"
+  },
+  {
+    "revision": "e24d7d737b8302d1216cacf5a8a02ed0",
+    "url": "/helfer-app/index.html"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/helfer-app/expo-service-worker.js"
+  },
+  {
+    "revision": "dcaeb12f332621f575c6538b03d13081",
+    "url": "/helfer-app/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "e741ccf1172af3c5c3244dacdffd322c",
+    "url": "/helfer-app/apple/icons/icon_192x192.png"
+  }
+];
