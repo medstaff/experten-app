9dc4d8de795f5ded42ce51b52bcf4c9cf95c5156
Jeremy Boy <github@jboy.eu>
2020-03-22 20:12:10 +0100

    doc

--- expo-app/App.tsx
+++ expo-app/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Navigator from "./src/navigation";
 
-
+/**
+ * Application entry point
+ *
+ * @constructor
+ */
 export default function App() {
     return <Navigator />
 }
